d6ce42ba9febe192909795e502dab72f5abb502d
stopvoldemort <david.squires1@gmail.com>
2017-09-03 11:16:26 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word_array.select do |test_word|
+      sort_letters(test_word) == sort_letters(self.word)
+    end
+  end
+
+  def sort_letters(word)
+    word.split("").sort.join
+  end
+
+end
